de7abe192af6896b6073b5bd0111d8a16507c7a5
nephente1 <nephente@interia.pl>
2018-06-04 15:58:08 +0200

    add style

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
   $(document).ready(function () {
     
     $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/eur/today/?format=json", function (data) {           
-                $('#currency_euro').html(data.currency);            
-                $('#output1a').html(data.rates[0].bid) ;               
+                $('#currency_euro').html(data.code);            
+                $('#output1a').html(data.rates[0].bid);
+                $('.data').html('Aktualizacja danych: '+ data.rates[0].effectiveDate);         
             })
 
 $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json", function (data) {           
-                $('#currency_dolar').html(data.currency);            
-                $('#output1b').html(data.rates[0].bid) ;               
+                $('#currency_dolar').html(data.code);            
+                $('#output1b').html(data.rates[0].bid) ;                         
             })
 
 $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/gbp/today/?format=json", function (data) {           
-              $('#currency_funt').html(data.currency);            
+              $('#currency_funt').html(data.code);            
               $('#output1c').html(data.rates[0].bid) ;               
+          })
+$.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/chf/today/?format=json", function (data) {           
+              $('#currency_frank').html(data.code);            
+              $('#output1d').html(data.rates[0].bid);     
           })
     
     $("#btn").click(function () {
       let wpis = document.querySelector('input').value;
       //pobierz euro
         $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/eur/today/?format=json", function (data) {           
-            $('#output1').html( wpis / data.rates[0].bid) ;              
+            let euro = wpis / data.rates[0].bid;
+            $('#output1').html(euro.toFixed(3) + ' EUR') ;              
         })
       //pobierz dolar
       $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json", function (data) {      
@@ -29,6 +35,10 @@ $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/gbp/today/?format=json",
       $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/gbp/today/?format=json", function (data) {      
             $('#output3').html(wpis / data.rates[0].bid);
         })
+        //pobierz franki
+      $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/chf/today/?format=json", function (data) {      
+        $('#output4').html(wpis / data.rates[0].bid);
+    })
     })
 
 })
