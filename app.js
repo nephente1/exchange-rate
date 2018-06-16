274294062c0fba215d177a790aadb202923b6c3d
nephente1 <nephente@interia.pl>
2018-06-16 08:47:27 +0200

    style changes and some test code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
   $(document).ready(function () {
     
-    $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/eur/today/?format=json", function (data) {           
+    $.get("https://api.nbp.pl/api/exchangerates/rates/c/eur/today/?format=json", function (data) {           
                 $('#currency_euro').html(data.code);            
                 $('#output1a').html(data.rates[0].bid);
                 $('.data').html('Aktualizacja danych: '+ data.rates[0].effectiveDate);         
             })
 
-$.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json", function (data) {           
+$.get("https://api.nbp.pl/api/exchangerates/rates/c/usd/today/?format=json", function (data) {           
                 $('#currency_dolar').html(data.code);            
                 $('#output1b').html(data.rates[0].bid) ;                         
             })
@@ -44,4 +44,44 @@ $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/chf/today/?format=json",
     })
     })
 
+    let pln = document.getElementById('pln').selected;
+    console.log(pln)
+    let eur = document.getElementById('eur').selected;
+    const select = $('select');
+    select.on('change', function() {
+        const selectVal = $(this).find('option:selected').val();//pobieramy wartość wybranego selekta
+    
+        if (selectVal != "PLN") {
+        console.log('działa');
+    }
 })
+    const apiURL = 'https://api.nbp.pl/api/exchangerates/rates/c/eur/today/?format=json';
+    
+    function insertData(bid) {
+        let euroKurs = $('.euroKurs');
+        let euroData = $('<p>'+bid+'</p>');
+        euroKurs.append(euroData);
+    }
+
+    function readData() {
+        $.ajax({
+            url : apiURL,
+            type: "get",
+            dataType : 'json'
+        })
+        .done(function(response){
+            console.log(response.rates[0].bid);//sprawdzam w konsoli zawartosc obiektu jsona
+             insertData(response.rates[0].bid);
+         })
+         .fail(function(err){
+             console.log(err);
+            })
+    }
+   
+    console.log(readData());
+    
+    localStorage.setItem('itemName','Hello from localStorage!');
+    var data = localStorage.getItem('itemName');
+    console.log(data)
+
+});
