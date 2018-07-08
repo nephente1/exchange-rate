2d88971063877d044e6015191d0a632b44381042
nephente1 <nephente@interia.pl>
2018-07-08 17:24:17 +0200

    working select

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
   $(document).ready(function () {
-    
-    $.get("https://api.nbp.pl/api/exchangerates/rates/c/eur/?format=json", function (data) {           
-                $('#currency_euro').html(data.code);            
-                $('#output1a').html(data.rates[0].bid.toFixed(2));
-                $('.data').html('Aktualizacja danych: '+ data.rates[0].effectiveDate);         
-            })
 
-$.get("https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json", function (data) {           
+    //pobranie daty danych z api
+    $.get("https://api.nbp.pl/api/exchangerates/rates/c/eur/?format=json", function (data) {           
+        $('.data').html('Aktualizacja danych: '+ data.rates[0].effectiveDate);         
+    })
+
+    //ustalenie selecta na wejściu
+    $("select").val("PLN");
+
+    document.querySelector('input').setAttribute('placeholder', 'kwota w PLN')
+    // if (select.value === "PLN") {    
+$.get("https://api.nbp.pl/api/exchangerates/rates/c/eur/?format=json", function (data) {           
+        $('#currency_euro').html(data.code);            
+        $('#output1a').html(data.rates[0].bid.toFixed(2));
+        $('.data').html('Aktualizacja danych: '+ data.rates[0].effectiveDate);         
+    })
+
+$.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json", function (data) {           
                 $('#currency_dolar').html(data.code);            
                 $('#output1b').html(data.rates[0].bid.toFixed(2)) ;                         
             })
@@ -21,41 +31,52 @@ $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/chf/?format=json", funct
           })
     
     $("#btn").click(function () {
-      let wpis = document.querySelector('input').value;
-      //pobierz euro
-        $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/eur/?format=json", function (data) {           
-            let euro = wpis / data.rates[0].bid;
-            $('#output1').html(euro.toFixed(3) + ' EUR') ;              
-        })
-      //pobierz dolar
-      $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/usd/?format=json", function (data) {      
-            let dolar = wpis / data.rates[0].bid
-            $('#output2').html(dolar.toFixed(3) + ' USD');                
-        })
-            //pobierz funty
-      $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/gbp/?format=json", function (data) {      
-            let funt = wpis / data.rates[0].bid
-            $('#output3').html(funt.toFixed(3) + ' GPB'); 
-        })
-        //pobierz franki
-      $.getJSON("https://api.nbp.pl/api/exchangerates/rates/c/chf/?format=json", function (data) {      
-            let frank = wpis / data.rates[0].bid
-            $('#output4').html(frank.toFixed(3) + ' CHF'); 
-    })
+        let wpis = document.querySelector('input').value;
+      //wynik euro  
+        let euroSum = parseFloat(document.getElementById('output1a').innerText);
+        let euro = wpis / euroSum;
+        $('#output1').html(euro.toFixed(2) + ' EUR'); 
+      //wynik dolar
+        let dolarSum = parseFloat(document.getElementById('output1b').innerText);   
+        let dolar = wpis / dolarSum;
+        $('#output2').html(dolar.toFixed(2) + ' USD');                
+      //wynik funty
+        let funtSum = parseFloat(document.getElementById('output1c').innerText);    
+        let funt = wpis / funtSum;
+        $('#output3').html(funt.toFixed(2) + ' GPB'); 
+      //wynik franki
+        let frankSum = parseFloat(document.getElementById('output1d').innerText);     
+        let frank = wpis / frankSum;
+        $('#output4').html(frank.toFixed(2) + ' CHF'); 
     })
 
-    let pln = document.getElementById('pln').selected;
-    console.log(pln)
-    let eur = document.getElementById('eur').selected;
     const select = $('select');
     select.on('change', function() {
-        const selectVal = $(this).find('option:selected').val();//pobieramy wartość wybranego selekta
+        var selectVal = $(this).find('option:selected').val();//pobieramy wartość wybranego selekta
     
         if (selectVal != "PLN") {
-        console.log('działa');
-    }
+
+            document.querySelector('#output1').innerText = ''
+            document.querySelector('input').value = '';
+            document.querySelector('input').setAttribute('placeholder', 'kwota EURO')
+            console.log('dziala EU');
+            $.get("https://api.nbp.pl/api/exchangerates/rates/c/eur/?format=json", function (data) {           
+                $('#currency_euro').html('PLN');            
+                $('#output1a').html(data.rates[0].ask.toFixed(2));         
+            })
+            
+            $("#btn").click(function () {
+                let wpis = document.querySelector('input').value;
+                
+              //wynik pln  
+                let euroAsk = parseFloat(document.getElementById('output1a').innerText);
+                let plnSum = wpis * euroAsk;
+                $('#output1').html(plnSum.toFixed(2) + ' PLN'); 
+            })      
+        } //close of if selectVal
+        else{
+            location.reload();
+        }
+    })
+
 })
-
-    
-
-});
